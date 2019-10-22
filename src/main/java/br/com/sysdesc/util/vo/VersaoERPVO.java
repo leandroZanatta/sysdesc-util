a4fe7f8a2673dae7082473e7645ea4b6eb60adf6
Leandro <Leandro@DESKTOP-55MSHCM>
2019-10-21 22:16:11 -0300

    ajuste versão

---
+++ src/main/java/br/com/sysdesc/util/vo/VersaoERPVO.java
@@ -0,0 +1,35 @@
+package br.com.sysdesc.util.vo;
+
+public class VersaoERPVO {
+
+	private String versaoERP = "0.0.0";
+
+	private String arquivoERP;
+
+	private String arquivoSysdesc;
+
+	public String getVersaoERP() {
+		return versaoERP;
+	}
+
+	public void setVersaoERP(String versaoERP) {
+		this.versaoERP = versaoERP;
+	}
+
+	public String getArquivoERP() {
+		return arquivoERP;
+	}
+
+	public void setArquivoERP(String arquivoERP) {
+		this.arquivoERP = arquivoERP;
+	}
+
+	public String getArquivoSysdesc() {
+		return arquivoSysdesc;
+	}
+
+	public void setArquivoSysdesc(String arquivoSysdesc) {
+		this.arquivoSysdesc = arquivoSysdesc;
+	}
+
+}

549f4202d8df772d3325158f4a9e4feda6b1bdd1
Leandro <Leandro@DESKTOP-55MSHCM>
2019-11-30 15:08:40 -0200

    data inicial

--- src/main/java/br/com/sysdesc/util/classes/DateUtil.java
+++ src/main/java/br/com/sysdesc/util/classes/DateUtil.java
@@ -2,12 +2,15 @@ package br.com.sysdesc.util.classes;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public class DateUtil {
 
 	public static final String FORMATO_DD_MM_YYY = "dd/MM/yyy";
 	public static final String FORMATO_DD_MM_YYYY_HH_MM_SS = "dd/MM/yyyy hh:mm:ss";
+	private static final ZoneId FUSO_HORARIO = ZoneId.systemDefault();
 
 	public static Date parse(String formato, String valor) {
 
@@ -21,5 +24,13 @@ public class DateUtil {
 	public static String format(String formato, Date valor) {
 
 		return new SimpleDateFormat(formato).format(valor);
+	}
+
+	public static Date getInitialDate(Date data) {
+
+		LocalDateTime currentDateTime = LocalDateTime.ofInstant(data.toInstant(), FUSO_HORARIO);
+
+		return Date.from(
+				currentDateTime.withHour(0).withMinute(0).withSecond(0).withNano(0).atZone(FUSO_HORARIO).toInstant());
 	}
 }
